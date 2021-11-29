b40ae5da1c14e1add7b0803ab373b8f911bf093c
hopeht909 <hopeht909@gmail.com>
2021-11-29 17:02:48 +0000

    Cold Call

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var buttonCall: UIButton!
+    
+    let nameList: [String] = ["Uyanga", "Courtny" ,"Jay" , "Bryant", "Jimmy" , "Cody", "Ryota"]
+    
+    var currentName = 0
+    
+    @IBAction func buttonCallPressed(_ sender: UIButton) {
+        if(currentName < nameList.count - 1){
+                   currentName += 1
+               }
+              
+               else{
+                   currentName = 0
+               }
+               updateLable()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateLable()
     }
-
-
+    func updateLable(){
+        nameLabel.text = nameList[currentName]
+    }
 }
 
